9d1d65fb0681b0525fca9f1c07414f00054ca2e9
Shakir Ali <shakiraliswe@gmail.com>
2023-02-08 14:29:35 +0500

    v1 - changes

--- routes/index.js
+++ routes/index.js
@@ -15,22 +15,28 @@ router.post("/contacts", async (req, res) => {
 router.post("/save-contact", async (req, res) => {
   let data = req.body;
   let files = req.files;
+  try {
+    if (files && files.profile) {
+      const profile = files.profile;
+      const profileName = "profile/" + uniqString() + ".png";
+      profile.mv(`./public/${profileName}`);
+      data = { ...data, profileURL: profileName };
+    } else {
+      data = { ...data, profileURL: null };
+    }
 
-  if (files && files.profile) {
-    const profile = files.profile;
-    const profileName = "profile/" + uniqString() + ".png";
-    profile.mv(`./public/${profileName}`);
-    data = { ...data, profileURL: profileName };
-  } else {
-    data = { ...data, profileURL: null };
-  }
-
-  saveContact(data, () => {
-    return res.status(200).send({
-      message: `Record added successfully`,
-      data: data,
+    saveContact(data, () => {
+      return res.status(200).send({
+        message: `Record added successfully`,
+        data: data,
+      });
     });
-  });
+  } catch (err) {
+    return res.status(403).send({
+      message: `Error ${err.message}`,
+      data: [],
+    });
+  }
 });
 
 router.get("*", (req, res) => {
